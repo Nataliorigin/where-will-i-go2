aada25e3504d51a9874c825d18c4da21096c6d5e
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-11-07 20:47:50 +0500

    ...

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
     $('.header__burger,.header__menu').toggleClass('active');
   });
 });
+
+const square = document.querySelector('.item-people');
+square.classList.remove('square-transition');
+
+// Добавить наблюдение за появлением элемента
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('ani');
+      return;
+    }
+    square.classList.remove('ani');
+  });
+});
+observer.observe(document.querySelector('.people__columns'));
