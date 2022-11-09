df51d2a32ed9e94326f2d36c07653264a6f9b23c
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-11-10 04:44:50 +0500

    ок

--- js/script.js
+++ js/script.js
@@ -4,18 +4,35 @@ $(document).ready(function () {
   });
 });
 
-const item = document.querySelector('.people__columns');
-item.classList.remove('ani');
+// const item = document.querySelector('.people__columns');
+// item.classList.remove('ani');
 
-// Добавить наблюдение за появлением элемента
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      item.classList.add('ani');
-      return;
+// // Добавить наблюдение за появлением элемента
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       item.classList.add('ani');
+//       return;
+//     }
+//     item.classList.remove('ani');
+//   });
+// });
+// observer.observe(document.querySelector('.people__columns'));
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("ani");
     }
-    item.classList.remove('ani');
   });
-});
-observer.observe(document.querySelector('.people__columns'));
+}
 
+let options = {
+  threshold: [0.4],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".item-people");
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
